09e17aae5f9ed5b5b87f087ce3f3455917f2192b
mydta <tdmy2505@gmail.com>
2025-03-02 18:07:52 +0700

    Fix deployment by renaming build/web to public

--- build.sh
+++ build.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 
+# Exit on errors
+set -e
+
 # Download and extract Flutter
 curl -fsSL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.19.0-stable.tar.xz | tar -xJ
 
 # Add Flutter to PATH
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Fix dubious ownership error
+# Fix ownership issue in Vercel
 git config --global --add safe.directory /vercel/path0/flutter
 
 # Ensure Flutter is installed and updated
-flutter doctor --no-root
-flutter upgrade --no-root
+flutter doctor
+flutter upgrade
 
-# Get dependencies
-flutter pub get --no-root
+# Install dependencies
+flutter pub get
 
-# Build the Flutter Web app
+# Build Flutter Web
 flutter build web
+
+# Rename the output folder to 'public' for Vercel
+mv build/web public
